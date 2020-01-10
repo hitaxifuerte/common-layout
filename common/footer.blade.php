c51959d785d5d096501095c114fff0e16ea9ed3f
hitaxifuerte <58800868+hitaxifuerte@users.noreply.github.com>
2020-01-10 10:25:06 +0530

    Add files via upload

---
+++ common/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer fixed">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">© {{ now()->year }} Visitors. All rights reserved | Design by FUERTE DEVELOPERS </span>
+    
+  </div>
+</footer>
---
+++ common/sidebar.blade.php
@@ -0,0 +1,58 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    
+    <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/dashboard') }}">
+        <span class="menu-title">Dashboard</span>
+        <i class="mdi mdi-home menu-icon"></i>
+      </a>
+    </li>
+    
+    <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/client') }}">
+        <span class="menu-title">Client</span>
+     <i class="mdi mdi-contacts menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/subscribenow') }}">
+        <span class="menu-title">Subscribe</span>
+        <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/fuerteteam') }}">
+        <span class="menu-title">Fuerte Team</span>
+        <i class="mdi mdi-chart-bar menu-icon"></i>
+      </a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/farmersurvey') }}">
+        <span class="menu-title">Farmer Survey </span>
+        <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+      </a>
+    </li>
+    
+    <!--<li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
+        <span class="menu-title">Farmer</span>
+        <i class="menu-arrow"></i>
+        <i class="mdi mdi-medical-bag menu-icon"></i>
+      </a>
+      <div class="collapse" id="general-pages">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html">  Farmer Blog</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Farmer Survey </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html">Farmer Story </a></li>
+          
+        </ul>
+      </div>
+    </li>=-->
+     <li class="nav-item">
+      <a class="nav-link" href="{{ url('admin/projectarea') }}">
+        <span class="menu-title">Project Area</span>
+       <i class="mdi mdi-medical-bag menu-icon"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
